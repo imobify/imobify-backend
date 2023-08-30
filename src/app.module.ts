7ddb260b088638a3c5da071c597452b070e3f220
Gabriel Lima <gabriel.limabs02@gmail.com>
2023-08-30 19:34:20 -0300

    refactor: moves PrismaModule into "shared" folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './shared/prisma/prisma.module';
 
 @Module({
   imports: [
