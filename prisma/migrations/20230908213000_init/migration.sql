9ab65dc99bd31ecec5fc1a670d5fd97b8d46d625
Gabriel Lima <gabriel.limabs02@gmail.com>
2023-09-08 19:36:01 -0300

    feat: implements publicId fields alongside image url's, cascade on delete, etc

--- prisma/migrations/20230908213000_init/migration.sql
+++ prisma/migrations/20230908213000_init/migration.sql
@@ -11,6 +11,7 @@ CREATE TABLE "user" (
     "document" TEXT NOT NULL,
     "hash" TEXT NOT NULL,
     "avatar_url" TEXT,
+    "avatar_public_id" TEXT,
     "type_id" INTEGER NOT NULL,
 
     CONSTRAINT "user_pkey" PRIMARY KEY ("id")
@@ -35,9 +36,9 @@ CREATE TABLE "real_estate" (
     "description" TEXT NOT NULL,
     "address" TEXT NOT NULL,
     "area" DOUBLE PRECISION NOT NULL,
-    "selling_value" DOUBLE PRECISION NOT NULL,
-    "renting_value" DOUBLE PRECISION NOT NULL,
-    "tax_value" DOUBLE PRECISION NOT NULL,
+    "selling_value" DOUBLE PRECISION,
+    "renting_value" DOUBLE PRECISION,
+    "tax_value" DOUBLE PRECISION,
     "coordinates" geometry(Point, 4326) NOT NULL,
     "isActive" BOOLEAN NOT NULL,
     "owner_id" TEXT NOT NULL,
@@ -73,6 +74,7 @@ CREATE TABLE "real_estate_photo" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "photoUrl" TEXT NOT NULL,
+    "photoPublicId" TEXT NOT NULL,
     "realEstate_id" INTEGER NOT NULL,
 
     CONSTRAINT "real_estate_photo_pkey" PRIMARY KEY ("id")
@@ -94,19 +96,19 @@ CREATE INDEX "location_idx" ON "real_estate" USING GIST ("coordinates");
 ALTER TABLE "user" ADD CONSTRAINT "user_type_id_fkey" FOREIGN KEY ("type_id") REFERENCES "user_type"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "real_estate" ADD CONSTRAINT "real_estate_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "real_estate" ADD CONSTRAINT "real_estate_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "lead" ADD CONSTRAINT "lead_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "lead" ADD CONSTRAINT "lead_realEstate_id_fkey" FOREIGN KEY ("realEstate_id") REFERENCES "real_estate"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "lead" ADD CONSTRAINT "lead_realEstate_id_fkey" FOREIGN KEY ("realEstate_id") REFERENCES "real_estate"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "favorite" ADD CONSTRAINT "favorite_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "favorite" ADD CONSTRAINT "favorite_realEstate_id_fkey" FOREIGN KEY ("realEstate_id") REFERENCES "real_estate"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "favorite" ADD CONSTRAINT "favorite_realEstate_id_fkey" FOREIGN KEY ("realEstate_id") REFERENCES "real_estate"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "real_estate_photo" ADD CONSTRAINT "real_estate_photo_realEstate_id_fkey" FOREIGN KEY ("realEstate_id") REFERENCES "real_estate"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "real_estate_photo" ADD CONSTRAINT "real_estate_photo_realEstate_id_fkey" FOREIGN KEY ("realEstate_id") REFERENCES "real_estate"("id") ON DELETE CASCADE ON UPDATE CASCADE;
