1c36f80e28c176c75000c0a354b96a4cbfdb8606
Gabriel Lima <gabriel.limabs02@gmail.com>
2023-12-02 16:00:41 -0300

    fix: removes JSON.parse attempt at deletedPhotos array, as each id is already a string

--- src/models/realEstate/dto/edit-real-estate.dto.ts
+++ src/models/realEstate/dto/edit-real-estate.dto.ts
@@ -1,11 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRealEstateDto } from './create-real-estate.dto';
 import { IsArray, IsOptional, IsString } from 'class-validator';
-import { Transform } from 'class-transformer';
 
 export class EditRealEstateDto extends PartialType(CreateRealEstateDto) {
   @IsOptional()
-  @Transform(({ value }) => JSON.parse(value))
   @IsArray()
   deletedPhotos?: string[];
 
